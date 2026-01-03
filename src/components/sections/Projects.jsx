41e5daefa8a3e35a2dea903d79a5439e1179e20b
srijalMdr <srijalmdr7@gmail.com>
2026-01-03 21:31:35 +0545

    adding pokemon and weather

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -43,6 +43,64 @@ function Projects() {
             </div>
 
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all ">
+              <h3 className="text-xl font-bold mb-2">Battle Pokemon</h3>
+              <p className="text-gray-400 mb-4">
+                Responsive and interactive Pokémon battle web app with
+                type-based search, real-time battles, and battle history
+                tracking for an engaging user experience.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "React Router", "React Query", "Tailwind CSS"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition  "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://srijalpokemon.netlify.app/"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all ">
+              <h3 className="text-xl font-bold mb-2">Weather App</h3>
+              <p className="text-gray-400 mb-4">
+                Responsive and visually engaging weather web app that lets users
+                search by city name and explore detailed hourly forecasts for
+                the coming days.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "React Query", "Tailwind CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition  "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://srijalweather.netlify.app/"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all ">
               <h3 className="text-xl font-bold mb-2">The Wild-Oasis</h3>
               <p className="text-gray-400 mb-4">
                 A hotel management web app for bookings, check-ins, cabin
